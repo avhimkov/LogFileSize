e9b1a1a1c20d61021a765247236585612f6f790e
Dmitry <avhimkov@mail.ru>
2017-03-28 16:57:47 +0500

    add unzip func

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"io"
 	"time"
+	"strings"
+	"archive/zip"
 )
 
 var Okno1 = "file/Окно №1"
@@ -146,4 +148,48 @@ func listfiles(rootpath string) []string {
 		fmt.Printf("walk error [%v]\n", err)
 	}
 	return list
+}
+
+func unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		fpath := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, f.Mode())
+		} else {
+			var fdir string
+			if lastIndex := strings.LastIndex(fpath,string(os.PathSeparator)); lastIndex > -1 {
+				fdir = fpath[:lastIndex]
+			}
+
+			err = os.MkdirAll(fdir, f.Mode())
+			if err != nil {
+				log.Fatal(err)
+				return err
+			}
+			f, err := os.OpenFile(
+				fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
